c8faac7e998f1abe1dc166e7e35aae247f06c399
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-05-07 17:45:23 +0700

    refactor App.js use react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
-import Navigation from "../src/components/Navbar";
-import Jumbotron from "../src/components/Header";
-import Content from "../src/components/Content";
-import Survey1 from "../src/components/Survey1";
-import Survey2 from "../src/components/Survey2";
-import Survey3 from "../src/components/Survey3";
-import SurveyResult from "../src/components/SurveyResult"
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Search from "./pages/Search";
+import Survey1 from "./pages/Survey1";
+import Survey2 from "./pages/Survey2";
+import Survey3 from "./pages/Survey3";
+import SurveyResult from "./pages/SurveyResult";
 
 function App() {
   return (
-    <React.Fragment>
-      <Navigation />
-      <Jumbotron />
-      <Content />
-      <Survey1 />
-      <Survey2 />
-      <Survey3 />
-      <SurveyResult />
-    </React.Fragment>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/search" component={Search} />
+        <Route path="/survey/1" component={Survey1} />
+        <Route path="/survey/2" component={Survey2} />
+        <Route path="/survey/3" component={Survey3} />
+        <Route path="/survey/result" component={SurveyResult} />
+      </Switch>
+    </Router>
   );
 }
 
