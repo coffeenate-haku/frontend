daab063fc4a5ea0f23574f9b366dc9c687eba721
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-05-08 17:36:01 +0700

    improve search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -15,19 +15,30 @@ const Div = styled.div`
   flex-wrap: wrap;
 `;
 const Card = styled.div`
-  margin: 50px 20px;
+  margin: 50px 5px;
   padding: 30px 30px 0px 30px;
   text-align: center;
   border-radius: 30px;
-  box-shadow: 0px 0px 30px 0px rgba(76, 58, 50, 0.9);
+`;
+
+const H5 = styled.h5`
+  margin-top: 20px;
 `;
 
 const Img = styled.img`
-  width: 200px;
+  width: 250px;
 `;
 
-const Heading = styled.h4`
+const H4 = styled.h4`
   margin: 50px 0px 0px 100px;
+`;
+
+const H44 = styled.h4`
+  margin-left: 100px;
+`;
+
+const Star = styled.img`
+  width: 100px;
 `;
 
 const best = [
@@ -55,21 +66,51 @@ const best = [
 
 const Search = props => {
   return (
-    <Container>
-      <SearchNav />
-      <Heading>Best Seller</Heading>
-      <Div>
-        {best.map((best, index) => {
-          return (
-            <Card key={index}>
-              <Img src={best.image} alt="" />
-              <h5>{best.title}</h5>
-              <Img src={best.rating} alt="" />
-            </Card>
-          );
-        })}
-      </Div>
-    </Container>
+    <React.Fragment>
+      <Container>
+        <SearchNav />
+        <H4>Best Seller</H4>
+        <Div>
+          {best.map((best, index) => {
+            return (
+              <Card key={index}>
+                <Img src={best.image} alt="" />
+                <H5>{best.title}</H5>
+                <Star src={best.rating} alt="" />
+              </Card>
+            );
+          })}
+        </Div>
+      </Container>
+      <Container>
+        <H44>Recommendation</H44>
+        <Div>
+          {best.map((best, index) => {
+            return (
+              <Card key={index}>
+                <Img src={best.image} alt="" />
+                <H5>{best.title}</H5>
+                <Star src={best.rating} alt="" />
+              </Card>
+            );
+          })}
+        </Div>
+      </Container>
+      <Container>
+        <H44>Nearest</H44>
+        <Div>
+          {best.map((best, index) => {
+            return (
+              <Card key={index}>
+                <Img src={best.image} alt="" />
+                <H5>{best.title}</H5>
+                <Star src={best.rating} alt="" />
+              </Card>
+            );
+          })}
+        </Div>
+      </Container>
+    </React.Fragment>
   );
 };
 
