db5e9e13c1e5dd265873f507380f89e9b2abdade
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-05-09 11:06:20 +0700

    delete unused page

--- src/pages/LoginPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import { FormikForm } from "../components/Login"
-
-export default class LoginPage extends React.Component {
-  render() {
-    return <FormikForm />
-  }
-}
--- src/pages/RegisterPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import FormRegister from "../components/Register"
-
-export default class RegisterPage extends React.Component {
-  render() {
-    return <FormRegister />
-  }
-}
