d5bba1471793f0aed5b9bd497a5dc180431d8eb8
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-05-09 11:02:06 +0700

    resize image file, fix tabs in nav link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
-import Coffee from "../assets/logos/logo-main.svg";
 import { NavLink } from "react-router-dom";
-import { Modal, Container, Row, Col, Tab, Tabs } from "react-bootstrap"
-import { FormikForm } from "../components/Login"
-import modal from "../assets/images/modal.jpg"
-import FormRegister from "../components/Register"
+import styled from "@emotion/styled";
 
+import Coffee from "../assets/logos/logo-main.svg";
+import {
+  Modal,
+  Container,
+  Row,
+  Col,
+  Tab,
+  Tabs,
+  Navbar,
+  Image
+} from "react-bootstrap";
+import { FormikForm } from "../components/Login";
+import modal from "../assets/thumbnail/modal.jpg";
+import FormRegister from "../components/Register";
 
 const style = {
   navStyle: {
@@ -23,7 +32,8 @@ const style = {
 
   link: {
     color: "#4C3A32",
-    fontSize: "24px"
+    fontSize: "24px",
+    marginRight: "50px"
   },
 
   specialLink: {
@@ -31,108 +41,122 @@ const style = {
     color: "#f5f1e8",
     fontSize: "24px",
     padding: "10px"
+  },
+
+  col: {
+    width: "400px"
+  },
+
+  tabs: {
+    flexDirection: "row",
+    width: "400px"
+  },
+
+  tab: {
+    width: "100px"
+  },
+
+  img: {
+    maxWidth: "350px"
   }
 };
+
+const Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 100px 0 75px;
+`;
+
+const LinkNav = styled.div`
+  margin-top: 30px;
+`;
 
 class Navigation extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      key: 'sign up',
-      lgshow: false,
-    }
-
+      key: "Sign",
+      lgshow: false
+    };
   }
 
   handleClose = () => {
-    this.setState({ lgshow: false })
-  }
+    this.setState({ lgshow: false });
+  };
 
   handleShow = () => {
-    this.setState({ lgshow:true })
-  }
+    this.setState({ lgshow: true });
+  };
   render() {
     return (
       <Navbar style={style.navStyle} collapseOnSelect sty>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse style={style.justify} id="responsive-navbar-nav">
-          {/* <NavLink to="/" style={style.link}>
-            Home
-          </NavLink> */}
-          <Navbar.Brand>
-            <img
-              style={style.logo}
-              alt=""
-              src={Coffee}
-              width="300"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          {/* <NavLink onClick={this.handleShow} style={style.link}>
-
-            Login
-          </NavLink> */}
-          <NavLink to="/search" style={style.link}>
-            Search
-          </NavLink>
-
-
-          <NavLink onClick={this.handleShow} style={style.specialLink}>
-            Sign in
-          </NavLink>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Div>
+            <div>
+              <Navbar.Brand>
+                <img
+                  style={style.logo}
+                  alt=""
+                  src={Coffee}
+                  width="300"
+                  className="d-inline-block align-top"
+                />
+              </Navbar.Brand>
+            </div>
+            <LinkNav>
+              <NavLink to="/search" style={style.link}>
+                Search
+              </NavLink>
+              <NavLink onClick={this.handleShow} style={style.specialLink}>
+                Sign in
+              </NavLink>
+            </LinkNav>
+          </Div>
         </Navbar.Collapse>
-        <Modal 
-        size='lg'
-        show={this.state.lgshow} 
-        onHide={this.handleClose}
-        >
+        <Modal size="lg" show={this.state.lgshow} onHide={this.handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Tabs
-              defaultActiveKey="Sign up"
+              style={style.tabs}
+              defaultActiveKey="Sign"
               id="controlled-tab-example"
               activeKey={this.state.key}
               onSelect={key => this.setState({ key })}
-              >
+            >
+              <Tab eventKey="Sign" title="Sign in">
+                <Container>
+                  <Row>
+                    <Col xs={12} md={7} style={style.col}>
+                      <Image src={modal} alt="modal" style={style.img} />
+                    </Col>
+                    <Col xs={12} md={4} style={style.col}>
+                      <FormikForm />
+                    </Col>
+                  </Row>
+                </Container>
+              </Tab>
 
               <Tab eventKey="Sign up" title="Sign up">
-                <Container> 
+                <Container>
                   <Row>
-                    <Col xs={12} md={7}>
-                      <img src={modal} alt="modal" width="400" />
+                    <Col xs={12} md={7} style={style.col}>
+                      <Image src={modal} alt="modal" style={style.img} />
                     </Col>
-                    <Col xs={12} md={4}>
+                    <Col xs={12} md={4} style={style.col}>
                       <FormRegister />
                     </Col>
                   </Row>
                 </Container>
               </Tab>
-
-              <Tab eventKey="Sign" title="Sign in">
-                <Container> 
-                  <Row>
-                  <Col xs={12} md={7}>
-                    <img src={modal} alt="modal" width="400"  />
-                    </Col>
-                    <Col xs={12} md={4}>
-                      
-                        <FormikForm />
-
-                    </Col>
-                   
-                  </Row>
-                </Container>
-              </Tab>
-
             </Tabs>
           </Modal.Body>
           <Modal.Footer>
-            <NavLink onClick={this.handleClose}>
-            close
-            </NavLink>
+            <NavLink onClick={this.handleClose}>close</NavLink>
           </Modal.Footer>
         </Modal>
       </Navbar>
