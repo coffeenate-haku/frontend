67468689a472bd7da214bb3eb8cdceeb59b89630
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-05-09 13:45:21 +0700

    connect register page to backend using axios

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,31 +1,74 @@
-import React from 'react'
+import React from "react"
+import axios from "axios"
 
 class FormRegister extends React.Component {
   state = {
-    data: {}
+    name: "",
+    username: "",
+    email: "",
+    password: ""
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const API_URL = `http://localhost:5210/users/register`
+    const user = {
+      name: this.state.name,
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    axios
+      .post(API_URL, user)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h1>Signup</h1>
         <fieldset>
-          <input placeholder="Name" type="text" name="name" />
+          <input
+            placeholder="Name"
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+          />
+
+          <input
+            placeholder="Username"
+            type="text"
+            name="username"
+            onChange={this.handleChange}
+          />
+
+          <input
+            placeholder="Email"
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+          />
+
+          <input
+            placeholder="Password"
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+          />
         </fieldset>
 
         <fieldset>
-          <input  placeholder="Username" type="text" name="username" />
+          <input type="submit" value="Register" />
         </fieldset>
-
-        <fieldset>
-          <input placeholder="Email" type="email" name="email" />
-        </fieldset>
-
-        <fieldset>
-          <input placeholder="Password" type="password" name="password" />
-        </fieldset>
-
-        <input type="submit" value="Register" />
       </form>
     )
   }
