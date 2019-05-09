c38ed3d678549e5b116a6893aec0158dc1617401
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-05-09 16:13:35 +0700

    add bcrypt to Register and add Login function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,35 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import styled from "@emotion/styled";
+import React from "react"
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import styled from "@emotion/styled"
+import axios from "axios"
 
 const Container = styled.div`
   display: grid;
-`;
+`
 
 const Button = styled.button`
   background-color: #4c3a32;
   color: #f5f1e8;
   border-style: none;
   padding: 5px 10px;
-`;
+`
 
 const style = {
   field: {
     marginBottom: "10px",
     padding: "5px 10px"
   }
-};
+}
 
 const validateEmail = values => {
-  let errors = {};
-  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-  if (!values.email) errors.email = "Required";
+  let errors = {}
+  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+  if (!values.email) errors.email = "Required"
   else if (!regex.test(values.email)) {
-    errors.email = "Invalid email address";
+    errors.email = "Invalid email address"
   }
-  return errors;
-};
+  return errors
+}
 
 export const FormikForm = () => {
   return (
@@ -39,9 +40,19 @@ export const FormikForm = () => {
         validate={validateEmail}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+            const API_URL = `http://localhost:5210/users/login`
+            const userLogin = {
+              email: values.email,
+              password: values.password
+            }
+
+            axios
+              .post(API_URL, userLogin)
+              .then(response => console.log(response))
+              .catch(error => console.log(error))
+
+            setSubmitting(false)
+          }, 400)
         }}
       >
         {({ isSubmitting }) => (
@@ -71,5 +82,5 @@ export const FormikForm = () => {
         )}
       </Formik>
     </Container>
-  );
-};
+  )
+}
