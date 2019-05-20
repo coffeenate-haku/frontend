fa1b1eb858400cb093e368b88a8a84df4ceaaf08
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-05-20 14:22:49 +0700

    redux store setup, fix issue

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,28 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-const initialState = {};
+
+// const initialState = {};
 const middleware = [thunk];
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+  // other store enhancers if any
+);
+const store = createStore(reducer, enhancer);
 
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // initialState,
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
